cf33d62a6b99cda79b0dc3bf1c75caac5034ffc6
zellahenderson <zhenders@smith.edu>
2015-02-09 23:08:30 -0800

    Make UI actually reflect what drink you pick

--- app/src/main/java/com/nmckinley/beveryman/MainActivity.java
+++ app/src/main/java/com/nmckinley/beveryman/MainActivity.java
@@ -23,6 +23,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
 
 public class MainActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -32,12 +33,6 @@ public class MainActivity extends ActionBarActivity
 
     /** Used to store the last screen title. For use in {@link #restoreActionBar()}. */
     private CharSequence mTitle;
-
-//    private ListView mDrawerList;
-//    private DrawerLayout mDrawerLayout;
-//    private ArrayAdapter<String> mAdapter;
-//    private ActionBarDrawerToggle mDrawerToggle;
-//    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,66 +46,7 @@ public class MainActivity extends ActionBarActivity
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
-
-//        mDrawerList = (ListView)findViewById(R.id.navList);
-//        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-//        mActivityTitle = getTitle().toString(); //TODO - does this work?
-//
-//        addDrawerItems();
-//        setupDrawer();
-//
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setHomeButtonEnabled(true);
     }
-
-//    private void addDrawerItems() {
-//        String[] drinkArray = { "Whiskey sour", "Rum and coke", "Long island iced tea"};
-//        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, drinkArray);
-//        mDrawerList.setAdapter(mAdapter);
-//
-//        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                //TODO: load fragment for drink
-//            }
-//        });
-//    }
-//
-//    private void setupDrawer() {
-//        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-//                R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
-//
-//            /** Called when a drawer has settled in a completely open state. */
-//            public void onDrawerOpened(View drawerView) {
-//                super.onDrawerOpened(drawerView);
-//                getSupportActionBar().setTitle("Select a drink");
-//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-//            }
-//
-//            /** Called when a drawer has settled in a completely closed state. */
-//            public void onDrawerClosed(View view) {
-//                super.onDrawerClosed(view);
-//                getSupportActionBar().setTitle(mActivityTitle);
-//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-//            }
-//        };
-//
-//        mDrawerToggle.setDrawerIndicatorEnabled(true);
-//        mDrawerLayout.setDrawerListener(mDrawerToggle);
-//    }
-//
-//    @Override
-//    protected void onPostCreate(Bundle savedInstanceState) {
-//        super.onPostCreate(savedInstanceState);
-//        // Sync the toggle state after onRestoreInstanceState has occurred.
-//        mDrawerToggle.syncState();
-//    }
-//
-//    @Override
-//    public void onConfigurationChanged(Configuration newConfig) {
-//        super.onConfigurationChanged(newConfig);
-//        mDrawerToggle.onConfigurationChanged(newConfig);
-//    }
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
@@ -119,19 +55,19 @@ public class MainActivity extends ActionBarActivity
         fragmentManager.beginTransaction()
                 .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
                 .commit();
-        //TODO: update fragment strings
+        //TODO: update fragment strings here?
     }
 
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                mTitle = getString(R.string.title1);
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
+                mTitle = getString(R.string.title2);
                 break;
             case 3:
-                mTitle = getString(R.string.title_section3);
+                mTitle = getString(R.string.title3);
                 break;
         }
     }
@@ -176,9 +112,11 @@ public class MainActivity extends ActionBarActivity
     public static class PlaceholderFragment extends Fragment {
 
         private static final String ARG_SECTION_NUMBER = "section_number";
+        private static int section;
 
         /** Returns a new instance of this fragment for the given section number. */
         public static PlaceholderFragment newInstance(int sectionNumber) {
+            section = sectionNumber;
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
@@ -193,7 +131,34 @@ public class MainActivity extends ActionBarActivity
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            setTextById(rootView);
+
             return rootView;
+        }
+
+        private void setTextById(View rootView) {
+            TextView mDrinkTitle = (TextView) rootView.findViewById(R.id.drink_title);
+            TextView mDrinkDescr = (TextView) rootView.findViewById(R.id.drink_description);
+            TextView mDrinkIngre = (TextView) rootView.findViewById(R.id.drink_ingredients);
+
+            switch (section) {
+                case 1:
+                    mDrinkTitle.setText(getString(R.string.title1));
+                    mDrinkDescr.setText(getString(R.string.description1));
+                    mDrinkIngre.setText(getString(R.string.ingredients1));
+                    break;
+                case 2:
+                    mDrinkTitle.setText(getString(R.string.title2));
+                    mDrinkDescr.setText(getString(R.string.description2));
+                    mDrinkIngre.setText(getString(R.string.ingredients2));
+                    break;
+                case 3:
+                    mDrinkTitle.setText(getString(R.string.title3));
+                    mDrinkDescr.setText(getString(R.string.description3));
+                    mDrinkIngre.setText(getString(R.string.ingredients3));
+                    break;
+            }
         }
 
         @Override
